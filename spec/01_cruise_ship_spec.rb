2b9182d52a77ed7c5d22fdedb8cdeef7cb257f4b
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-28 03:08:41 +0000

    Automatically backed up by Learn

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -13,5 +13,13 @@ describe "#select_winner" do
       }
 
     expect(select_winner(passengers)).to eq("Amanda Presley")
+    winner = " "
+    passengers.each do |suite,name|
+      if suite == :suite_a && name.start_with?("A")
+        winner = name 
+      end 
+    end 
+    
+    winner 
   end
 end
