1e379f2f2edf2412c6d130a315733f0dba9b4ba0
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-28 03:17:26 +0000

    Automatically backed up by Learn

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -23,3 +23,17 @@ describe "#select_winner" do
     winner 
   end
 end
+
+birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
+"Happe birthday # {kids_name}! you are now # {age} years old!"
+def 
+  Happy_birthday(birthday_kids)
+birthday_kids.each do 
+  |kids_name,age|
+  puts "happy Birthday # {kids_name}! you are now # {age} years old!"
+   end
+end 
